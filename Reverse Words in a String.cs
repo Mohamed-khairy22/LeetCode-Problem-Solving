4cc047ef1279f47ddd810f97ec5dd62d7612434c
mohamed-khairy <mohamedkhairy063036@gmail.com>
2024-09-21 09:31:23 +0300

    Reverse Words in a String another solve

--- Reverse Words in a String.cs
+++ Reverse Words in a String.cs
@@ -1,29 +1,15 @@
 public class Solution {
-    public string ReverseWords(string s) {
-    string s2 = "",temp="";
+    public string ReverseWords(string s){
 
-    for (int i =0;i< s.Length; i++)
-    {
-        if( (s[i] != ' '))
-        {
-            temp += s[i];
-            
-        }
-        if (temp.Length > 0 && (s[i] == ' ' ||i==s.Length-1))
-        {
-            if (s2 == "")
-                s2 += temp;
-            else
-                s2 += " " + temp;
-            temp = "";
-        }
-    }
-    string[] words =s2.Split(' ') ;
+    string s2 = "";
+   
+    string[] words =s.Split(' ') ;
     Array.Reverse(words);
     s2 = "";
     foreach (string word in words)
     {
-        
+        if(word.Contains(' ')||word=="")
+            continue;
         if(s2=="")
             s2 += word;
         else
