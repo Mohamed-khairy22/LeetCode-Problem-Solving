461f550a04dbe6077c41a85a743dcd8babdb8afd
mohamed-khairy <mohamedkhairy063036@gmail.com>
2024-09-22 10:22:07 +0300

    Greatest Common Divisor of Strings using recursion

---
+++ Greatest Common Divisor of Strings.cs
@@ -0,0 +1,7 @@
+if (str1+str2!=str2+str1)
+    return "";
+if(str1.Length==str2.Length) 
+    return str1;
+if (str1.Length > str2.Length) 
+    return GcdOfStrings(str1.Substring(str2.Length),str2);
+return GcdOfStrings(str1, str2.Substring(str1.Length));
