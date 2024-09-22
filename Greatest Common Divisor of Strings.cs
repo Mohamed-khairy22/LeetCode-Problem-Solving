844826cb3f31c6a3664bd344e1b5894ae81daf7e
mohamed-khairy <mohamedkhairy063036@gmail.com>
2024-09-22 10:26:08 +0300

    Greatest Common Divisor of Strings update

--- Greatest Common Divisor of Strings.cs
+++ Greatest Common Divisor of Strings.cs
@@ -1,7 +1,11 @@
-if (str1+str2!=str2+str1)
-    return "";
-if(str1.Length==str2.Length) 
-    return str1;
-if (str1.Length > str2.Length) 
-    return GcdOfStrings(str1.Substring(str2.Length),str2);
-return GcdOfStrings(str1, str2.Substring(str1.Length));
+public class Solution {
+    public string GcdOfStrings(string str1, string str2) {
+        if (str1+str2!=str2+str1)
+            return "";
+        if(str1.Length==str2.Length) 
+            return str1;
+        if (str1.Length > str2.Length) 
+            return GcdOfStrings(str1.Substring(str2.Length),str2);
+        return GcdOfStrings(str1, str2.Substring(str1.Length));
+    }
+}
